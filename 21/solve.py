5b78e1063dea12b86ae4a19289fa88152b8f04d9
Kim Junseo <53117830+cloneot@users.noreply.github.com>
2021-11-24 19:26:44 +0900

    Update solve.py

--- 21/solve.py
+++ 21/solve.py
@@ -7,7 +7,7 @@ avoid_addr = [0x0000000000401598, 0x00000000004016A7]
 
 state = proj.factory.entry_state()
 sm = proj.factory.simulation_manager(state)
-sm.explore(find = find_addr, avoid = avoid_addr)	# avoid 생략 가능
+sm.explore(find = find_addr, avoid = avoid_addr)
 
 if len(sm.found) > 0:
 	print(f'[{sm.found[0].posix.dumps(0)}]')
